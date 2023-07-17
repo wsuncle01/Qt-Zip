e0b6422e4adf789fe01e5387035dba38ef65528c
wsuncle01 <52230688+wsuncle01@users.noreply.github.com>
2023-07-16 22:45:24 -0400

    Update zip.cpp

--- zip.cpp
+++ zip.cpp
@@ -13,16 +13,6 @@ Zip::Zip()
 
 void Zip::zipfile(QString OutPutPath)
 {
-
-//    QFile file("C:/Users/黄辙/Desktop/1111/test.bin");
-//    if (!file.open(QIODevice::WriteOnly)) {
-//        qDebug() << "Cannot open file for writing: "
-//                 << qPrintable(file.errorString());
-//        return;
-//    }
-//    QDataStream out(&file);
-//    out.writeRawData(zDate,zDate.end()-zDate.begin());
-//    file.close();
     ziptree.preOrder();
     QFile file(OutPutPath);
     if (!file.open(QIODevice::WriteOnly)) {
@@ -41,36 +31,10 @@ void Zip::zipfile(QString OutPutPath)
 }
 
 
-
-//void Zip::test()
-//{
-//    QFile file("C:/Users/黄辙/Desktop/1111/test.bin");
-//    if (!file.open(QIODevice::WriteOnly | QIODevice::Append)) {
-//        qDebug() << "Cannot open file for writing: "
-//                  << qPrintable(file.errorString());
-//        return;
-//    }
-//    int len=3;
-//    const char* charArray = "\x00\x01\x99";
-//    QByteArray byteArray = QByteArray::fromRawData(charArray, len);
-//    QDataStream out(&file);
-//    out.writeRawData(byteArray,byteArray.end()-byteArray.begin());
-//    file.close();
-//}
-
 QByteArray Zip::getcrc32(QString data)
 {
     QByteArray crcval;
     crcval=calcFileCRC(data);
-    //qDebug()<<crcval;
-//    QFile file("C:/Users/黄辙/Desktop/1111/test.bin");
-//    if (!file.open(QIODevice::WriteOnly |QIODevice::Append)) {
-//        qDebug() << "Cannot open file for writing: "
-//                 << qPrintable(file.errorString());
-//    }
-//    QDataStream out(&file);
-//    out.writeRawData(crcval,crcval.end()-crcval.begin());
-//    file.close();
     return crcval;
 }
 
@@ -78,15 +42,6 @@ QByteArray Zip::getlength(QString data)
 {
     QByteArray length;
     length=fsize(data);
-    //qDebug()<<length;
-//    QFile file("C:/Users/黄辙/Desktop/1111/test.bin");
-//    if (!file.open(QIODevice::WriteOnly |QIODevice::Append)) {
-//        qDebug() << "Cannot open file for writing: "
-//                 << qPrintable(file.errorString());
-//    }
-//    QDataStream out(&file);
-//    out.writeRawData(length,length.end()-length.begin());
-//    file.close();
     return length;
 }
 
